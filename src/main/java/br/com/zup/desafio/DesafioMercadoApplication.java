fd013b32814a1019915263c2fb16aa16bf12eaa8
Iris <iris.souza@zup.com.br>
2021-03-10 14:08:13 -0300

    Iniciar projeto

---
+++ src/main/java/br/com/zup/desafio/DesafioMercadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMercadoApplication.class, args);
+	}
+
+}
