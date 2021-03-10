5b32640b56f9e54748b1c791e36a390001c711ad
Iris <iris.souza@zup.com.br>
2021-03-10 16:06:08 -0300

    Adicionar classe de exceções

---
+++ src/main/java/br/com/zup/desafio/exceptions/ManipuladorDeExcecoes.java
@@ -0,0 +1,29 @@
+package br.com.zup.desafio.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ManipuladorDeExcecoes extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleMethodArgumentNotValid(ex, headers, status, request);
+    }
+
+    private List<ObjetoDeErro> getErros(MethodArgumentNotValidException exception){
+        List<ObjetoDeErro> objetosDeErro = exception.getBindingResult()
+                .getFieldErrors().stream()
+                .map(erro -> new ObjetoDeErro(erro.getDefaultMessage(), erro.getField()))
+                .collect(Collectors.toList());
+
+        return objetosDeErro;
+    }
+
+}
