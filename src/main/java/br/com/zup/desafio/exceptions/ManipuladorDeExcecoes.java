00e1895b019c25968a8c533582d4e9f346863d93
Iris <iris.souza@zup.com.br>
2021-03-11 13:23:32 -0300

    Adicionar ExceptionHandler

--- src/main/java/br/com/zup/desafio/exceptions/ManipuladorDeExcecoes.java
+++ src/main/java/br/com/zup/desafio/exceptions/ManipuladorDeExcecoes.java
@@ -4,17 +4,26 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@RestControllerAdvice
 public class ManipuladorDeExcecoes extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return super.handleMethodArgumentNotValid(ex, headers, status, request);
+        List<ObjetoDeErro> objetosDeErro = getErros(ex);
+        RespostaDeErro respostaDeErro = new RespostaDeErro("Validação", status.value(),
+                status.getReasonPhrase(), objetosDeErro);
+
+        return ResponseEntity.status(status).body(respostaDeErro);
     }
 
     private List<ObjetoDeErro> getErros(MethodArgumentNotValidException exception){
@@ -25,5 +34,47 @@ public class ManipuladorDeExcecoes extends ResponseEntityExceptionHandler {
 
         return objetosDeErro;
     }
+
+    @ExceptionHandler({ExcecaoBasica.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public RespostaDeErro manipularRuntimeException(ExcecaoBasica erro){
+        ObjetoDeErro objetoDeErro = new ObjetoDeErro(erro.getMessage(), erro.getCampo());
+        RespostaDeErro respostaDeErro = new RespostaDeErro(erro.getTipoDoErro(), erro.getStatus(), erro.getRazaoDoErro(),
+                Arrays.asList(objetoDeErro));
+
+        return respostaDeErro;
+    }
+
+    @ExceptionHandler({ClienteExistenteException.class})
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public RespostaDeErro manipularClienteExistenteException(ExcecaoBasica erro){
+        ObjetoDeErro objetoDeErro = new ObjetoDeErro(erro.getMessage(), erro.getCampo());
+        RespostaDeErro respostaDeErro = new RespostaDeErro(erro.getTipoDoErro(), erro.getStatus(), erro.getRazaoDoErro(),
+                Arrays.asList(objetoDeErro));
+
+        return respostaDeErro;
+    }
+
+    @ExceptionHandler({ClienteNaoEncontradoException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public RespostaDeErro manipularClienteNaoEncontrado(ExcecaoBasica erro){
+        ObjetoDeErro objetoDeErro = new ObjetoDeErro(erro.getMessage(), erro.getCampo());
+        RespostaDeErro respostaDeErro = new RespostaDeErro(erro.getTipoDoErro(), erro.getStatus(), erro.getRazaoDoErro(),
+                Arrays.asList(objetoDeErro));
+
+        return respostaDeErro;
+    }
+
+    @ExceptionHandler({ClienteExistenteEmailException.class})
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public RespostaDeErro manipularClienteExistente(ExcecaoBasica erro){
+        ObjetoDeErro objetoDeErro = new ObjetoDeErro(erro.getMessage(), erro.getCampo());
+        RespostaDeErro respostaDeErro = new RespostaDeErro(erro.getTipoDoErro(), erro.getStatus(), erro.getRazaoDoErro(),
+                Arrays.asList(objetoDeErro));
+
+        return respostaDeErro;
+    }
+
+
 
 }
