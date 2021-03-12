675cecdf97d35a52ec1b05691547af6648c77213
Iris <iris.souza@zup.com.br>
2021-03-11 21:30:10 -0300

    Adicionar classe ProdutoDTO

---
+++ src/main/java/br/com/zup/desafio/DTOs/ProdutoDTO.java
@@ -0,0 +1,20 @@
+package br.com.zup.desafio.DTOs;
+
+public class ProdutoDTO {
+    private String nome;
+
+    public ProdutoDTO(String nome) {
+        this.nome = nome;
+    }
+
+    public ProdutoDTO() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
