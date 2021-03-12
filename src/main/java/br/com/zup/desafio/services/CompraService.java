3652a2010cb48cb76ec39c07916956cc14d7881f
Iris <iris.souza@zup.com.br>
2021-03-11 21:32:02 -0300

    Adicionar classe de serviço CompraService

---
+++ src/main/java/br/com/zup/desafio/services/CompraService.java
@@ -0,0 +1,19 @@
+package br.com.zup.desafio.services;
+
+import br.com.zup.desafio.models.Compra;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CompraService {
+
+    List<Compra> compras = new ArrayList<>();
+
+    public Compra realizarCompra(Compra compra){
+        compras.add(compra);
+        return compra;
+    }
+
+}
